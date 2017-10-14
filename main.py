472f231e3cdeaeba9e4b9ea2a426929944fe0cd8
Varun Agrawal <varagrawal@gmail.com>
2017-10-13 21:06:46 -0400

    factory functions for transforms, models and losses

--- main.py
+++ main.py
@@ -23,52 +23,22 @@ import argparser
 # TODO: finetuning DTD
 # TODO: unmatch the sketch/texture input patch location for DTD
 
-def main(args):
-    layers_map = {'relu4_2': '22', 'relu2_2': '8', 'relu3_2': '13'}
-
-    vis = visdom.Visdom(port=args.display_port)
-
-    loss_graph = {
-        "g": [],
-        "gd": [],
-        "gf": [],
-        "gpl": [],
-        "gpab": [],
-        "gs": [],
-        "d": [],
-    }
-
-    # for rgb the change is to feed 3 channels to D instead of just 1. and feed 3 channels to vgg.
-    # can leave pixel separate between r and gb for now. assume user use the same weights
+def get_transforms(args):
+    transforms_list = [
+        custom_transforms.RandomSizedCrop(args.image_size, args.resize_min, args.resize_max),
+        custom_transforms.RandomHorizontalFlip(),
+        custom_transforms.toTensor()
+    ]
     if args.color_space == 'lab':
-        transform = custom_transforms.Compose([
-            custom_transforms.RandomSizedCrop(args.image_size, args.resize_min, args.resize_max),
-            custom_transforms.RandomHorizontalFlip(),
-            custom_transforms.toLAB(),
-            custom_transforms.toTensor()
-        ])
-
+        transforms_list.insert(2, custom_transforms.toLAB())
     elif args.color_space == 'rgb':
-        transform = custom_transforms.Compose([
-            custom_transforms.RandomSizedCrop(args.image_size, args.resize_min, args.resize_max),
-            custom_transforms.RandomHorizontalFlip(),
-            custom_transforms.toRGB('RGB'),
-            custom_transforms.toTensor()
-        ])
-        args.pixel_weight_ab = args.pixel_weight_rgb
-        args.pixel_weight_l = args.pixel_weight_rgb
+        transforms_list.insert(2, custom_transforms.toRGB('RGB'))
 
-    rgbify = custom_transforms.toRGB()
-    trainDset = ImageFolder('train', args.data_path, transform)
-    trainLoader = DataLoader(dataset=trainDset, batch_size=args.batch_size, shuffle=True)
+    transforms = custom_transforms.Compose(transforms_list)
+    return transforms
 
-    valDset = ImageFolder('val', args.data_path, transform)
-    indices = torch.randperm(len(valDset))
-    val_display_size = args.batch_size
-    val_display_sampler = SequentialSampler(indices[:val_display_size])
-    valLoader = DataLoader(dataset=valDset, batch_size=val_display_size, sampler=val_display_sampler)
-    # renormalize = transforms.Normalize(mean=[+0.5+0.485, +0.5+0.456, +0.5+0.406], std=[0.229, 0.224, 0.225])
 
+def get_models(args):
     sigmoid_flag = 1
     if args.gan == 'lsgan':
         sigmoid_flag = 0
@@ -89,25 +59,29 @@ def main(args):
         netD = discriminator.Discriminator(1, 32, sigmoid_flag)
     elif args.color_space == 'rgb':
         netD = discriminator.Discriminator(3, 32, sigmoid_flag)
-    feat_model = models.vgg19(pretrained=True)
+
     if args.load == -1:
         netG.apply(weights_init)
     else:
-
         load_network(netG, 'G', args.load, args.load_dir)
         print('Loaded G from itr:' + str(args.load))
+
     if args.load_D == -1:
         netD.apply(weights_init)
     else:
         load_network(netD, 'D', args.load_D, args.load_dir)
         print('Loaded D from itr:' + str(args.load_D))
 
+    return netG, netD
+
+
+def get_criterions(args):
     if args.gan == 'lsgan':
         criterion_gan = nn.MSELoss()
     elif args.gan == 'dcgan':
         criterion_gan = nn.BCELoss()
     else:
-        raise Warning("Undefined GAN type. Defaulting to LSGAN")
+        print("Undefined GAN type. Defaulting to LSGAN")
         criterion_gan = nn.MSELoss()
 
     # criterion_l1 = nn.L1Loss()
@@ -116,10 +90,47 @@ def main(args):
     criterion_style = nn.L1Loss()
     criterion_feat = nn.L1Loss()
 
-    input_stack = torch.FloatTensor()
-    target_img = torch.FloatTensor()
-    target_texture = torch.FloatTensor()
-    segment = torch.FloatTensor()
+    return criterion_gan, criterion_pixel_l, criterion_pixel_ab, criterion_style, criterion_feat
+
+
+def main(args):
+    layers_map = {'relu4_2': '22', 'relu2_2': '8', 'relu3_2': '13'}
+
+    vis = visdom.Visdom(port=args.display_port)
+
+    loss_graph = {
+        "g": [],
+        "gd": [],
+        "gf": [],
+        "gpl": [],
+        "gpab": [],
+        "gs": [],
+        "d": [],
+    }
+
+    # for rgb the change is to feed 3 channels to D instead of just 1. and feed 3 channels to vgg.
+    # can leave pixel separate between r and gb for now. assume user use the same weights
+    transforms = get_transforms(args)
+
+    if args.color_space == 'rgb':
+        args.pixel_weight_ab = args.pixel_weight_rgb
+        args.pixel_weight_l = args.pixel_weight_rgb
+
+    rgbify = custom_transforms.toRGB()
+    trainDset = ImageFolder('train', args.data_path, transform)
+    trainLoader = DataLoader(dataset=trainDset, batch_size=args.batch_size, shuffle=True)
+
+    valDset = ImageFolder('val', args.data_path, transform)
+    indices = torch.randperm(len(valDset))
+    val_display_size = args.batch_size
+    val_display_sampler = SequentialSampler(indices[:val_display_size])
+    valLoader = DataLoader(dataset=valDset, batch_size=val_display_size, sampler=val_display_sampler)
+    # renormalize = transforms.Normalize(mean=[+0.5+0.485, +0.5+0.456, +0.5+0.406], std=[0.229, 0.224, 0.225])
+
+    feat_model = models.vgg19(pretrained=True)
+    netG, netD = get_models(args)
+
+    criterion_gan, criterion_pixel_l, criterion_pixel_ab, criterion_style, criterion_feat = get_criterions(args)
 
     label = torch.FloatTensor(args.batch_size)
     real_label = 1
@@ -136,11 +147,30 @@ def main(args):
         criterion_pixel_l.cuda()
         criterion_pixel_ab.cuda()
         criterion_feat.cuda()
-        input_stack, target_img, target_texture, segment, label = input_stack.cuda(), target_img.cuda(), target_texture.cuda(), segment.cuda(), label.cuda()
+        
+        input_stack = torch.FloatTensor().cuda()
+        target_img = torch.FloatTensor().cuda()
+        target_texture = torch.FloatTensor().cuda()
+        segment = torch.FloatTensor().cuda()
 
-        Extract_content = FeatureExtractor(feat_model.features, [layers_map[args.content_layers]])
-        Extract_style = FeatureExtractor(feat_model.features,
+        extract_content = FeatureExtractor(feat_model.features, [layers_map[args.content_layers]])
+        extract_style = FeatureExtractor(feat_model.features,
                                          [layers_map[x.strip()] for x in args.style_layers.split(',')])
+
+        model = {
+            "netG": netG,
+            "netD": netD,
+            "criterion_gan": criterion_gan,
+            "criterion_pixel_l": criterion_pixel_l,
+            "criterion_pixel_ab": criterion_pixel_ab,
+            "criterion_feat": criterion_feat,
+            "criterion_style": criterion_style,
+            "real_label": real_label,
+            "fake_label": fake_label,
+            "optimizerD": optimizerD,
+            "optimizerG": optimizerG
+        }
+
         for epoch in range(args.num_epoch):
             for i, data in enumerate(trainLoader, 0):
 
@@ -256,17 +286,17 @@ def main(args):
                 err_pixel_ab = args.pixel_weight_ab * criterion_pixel_ab(outputab, targetab)
 
                 ##################feature Loss############################
-                out_feat = Extract_content(renormalize(outputlll))[0]
+                out_feat = extract_content(renormalize(outputlll))[0]
 
-                gt_feat = Extract_content(renormalize(gtlll))[0]
+                gt_feat = extract_content(renormalize(gtlll))[0]
                 err_feat = args.feature_weight * criterion_feat(out_feat, gt_feat.detach())
 
                 ##################style Loss############################
 
 
                 if args.local_texture_size == -1:  # global
-                    output_feat_ = Extract_style(outputlll)
-                    target_feat_ = Extract_style(targetlll)
+                    output_feat_ = extract_style(outputlll)
+                    target_feat_ = extract_style(targetlll)
                 else:
                     patchsize = args.local_texture_size
                     x = int(rand_between(patchsize, args.image_size - patchsize))
@@ -274,8 +304,8 @@ def main(args):
 
                     texture_patch = outputlll[:, :, x:(x + patchsize), y:(y + patchsize)]
                     gt_texture_patch = targetlll[:, :, x:(x + patchsize), y:(y + patchsize)]
-                    output_feat_ = Extract_style(texture_patch)
-                    target_feat_ = Extract_style(gt_texture_patch)
+                    output_feat_ = extract_style(texture_patch)
+                    target_feat_ = extract_style(gt_texture_patch)
 
                 gram = GramMatrix()
 
