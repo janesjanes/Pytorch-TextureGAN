12130c28258428c909011947c78f3828475ea4d8
Patsorn <patsorn_sangkloy@gatech.edu>
2017-11-07 05:06:17 +0000

    fix timing

--- train.py
+++ train.py
@@ -125,7 +125,7 @@ def gen_local_patch(patch_size, batch_size, eroded_seg,seg, img):
         while torch.sum(seg[i_bs,0,xstart:xend,ystart:yend]) < k*patch_size*patch_size:
                 
             try:
-                k = k*0.8
+                k = k*0.5
                 if len(seg_one) != 0:
                     random_select = int(rand_between(0, len(seg_one)-1))
             
